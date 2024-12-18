3e59f311e981ffa75b33a0dce66b19902622db22
Saeye <>
2024-12-18 10:26:25 +0900

    feat. base.html 눈내리는배경 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/",views.home, name="home")
+    path("kakao/", views.kakao, name="kakao"), 
+    path("", views.home, name="home"),    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def kakao(request):
+    return render(request, 'accounts/kakao.html')
+
 def home(request):
     return render(request, 'accounts/home.html')
